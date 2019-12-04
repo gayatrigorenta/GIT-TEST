cbc769d04da5b5f8823ebd30a09b553e14e5be78
Gayatri <gayatri.polisetty@paysafe.com>
2019-12-04 15:15:27 -0800

    googlesearch commit

---
+++ GoogleSearch/PerformGoogleSearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace GoogleSearch
+{
+    [TestClass]
+    public class PerformGoogleSearch
+    {
+        [Test]
+        public void MySearch()
+        {
+            //Create the reference for our browser
+            IWebDriver driver = new FirefoxDriver();
+            //IWebDriver driver = base(driver);
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http:www.google.com");
+            //Find the Search text box UI Element
+            IWebElement element = driver.FindElement(By.Name("q"));
+            //Perform Ops
+            element.SendKeys("Hello");
+            element.FindElement(By.XPath("//div[@class='FPdoLc VlcLAe']//input[@name='btnK']")).Click();
+            //Close the browser
+            Thread.Sleep(5000);
+            driver.Close();
+
+        }
+    }
+}
---
+++ GoogleSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoogleSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87357696-73be-4329-b21e-e2cdec3e4a4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
